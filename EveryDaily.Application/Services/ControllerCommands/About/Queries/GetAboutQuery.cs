40a8be94ccb6bdd7dc1f9c30b0117ae5585a1179
Furkan <dfurkan5234@gmail.com>
2025-03-10 22:24:50 +0300

    fixed

--- EveryDaily.Application/Services/ControllerCommands/About/Queries/GetAboutQuery.cs
+++ EveryDaily.Application/Services/ControllerCommands/About/Queries/GetAboutQuery.cs
@@ -3,11 +3,6 @@ using EveryDaily.Core.Dtos;
 using EveryDaily.Persistence;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EveryDaily.Application.Services.ControllerCommands.About.Queries
 {
