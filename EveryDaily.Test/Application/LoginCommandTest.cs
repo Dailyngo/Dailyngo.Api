fa22740da528bb1d858570b5a8e1ca53c99e8046
OmerSoydemir <omerfaruksoydemirr@gmail.com>
2025-03-16 00:45:48 +0300

    LoginCommand Test bitti WrongUser ve Wrongpassword eklendi.

--- EveryDaily.Test/Application/LoginCommandTest.cs
+++ EveryDaily.Test/Application/LoginCommandTest.cs
@@ -80,7 +80,7 @@ namespace EveryDaily.Test.Application
                 _signInManagerMock.Object,
                 _jwtTokenGeneratorMock.Object
             );
-            
+
             await appDbContext.Users.AddAsync(userEntity);
             await appDbContext.SaveChangesAsync();
 
@@ -91,11 +91,11 @@ namespace EveryDaily.Test.Application
             _jwtTokenGeneratorMock
                 .Setup(x => x.GenerateToken(It.IsAny<UserEntity>()))
                 .Returns("generated-token");
-            
+
             _jwtTokenGeneratorMock
                 .Setup(x => x.GenerateRefreshToken(It.IsAny<UserEntity>()))
                 .ReturnsAsync("refresh-token");
-            
+
             // Act
             var result = await _handler.Handle(command, CancellationToken.None);
 
@@ -105,6 +105,94 @@ namespace EveryDaily.Test.Application
             Assert.IsNotNull(result.Data);
             Assert.That(result.Data.Token, Is.EqualTo("generated-token"));
             Assert.That(result.Data.RefreshToken, Is.EqualTo("refresh-token"));
+        }
+        [Test]
+        public async Task Handle_WrongPassword_Returns401()
+        {
+            var command = new LoginCommand
+            {
+                EmailOrUserName = "testuser@example.com",
+                Password = "wrongpassword"
+            };
+
+            var userEntity = new UserEntity
+            {
+                Name = "Test",
+                Surname = "User",
+                Email = "testuser@example.com",
+                IsDeleted = false,
+                CreatedAt = DateTimeOffset.Now
+            };
+
+            await using var appDbContext = new AppDbContext(SetupDefaultMoq.CreateDbContextOptions());
+            // Handler oluşturuluyor
+            _handler = new LoginCommandHandler(
+                appDbContext,
+                _signInManagerMock.Object,
+                _jwtTokenGeneratorMock.Object
+            );
+         
+            await appDbContext.Users.AddAsync(userEntity);
+            await appDbContext.SaveChangesAsync();
+
+            _signInManagerMock
+                .Setup(x => x.PasswordSignInAsync(It.IsAny<UserEntity>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>()))
+                .ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.Failed);
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.StatusCode, Is.EqualTo(400));
+            Assert.AreEqual(result.messages, "Invalid password");
+
+
+        }
+
+        [Test]
+        public async Task Handle_WrongUsername_Returns400()
+        {
+            var command = new LoginCommand
+            {
+                EmailOrUserName = "wronguser@example.com",
+                Password = "testpassword"
+            };
+
+            var userEntity = new UserEntity
+            {
+                Name = "Test",
+                Surname = "User",
+                Email = "testuser@example.com",
+                IsDeleted = false,
+                CreatedAt = DateTimeOffset.Now
+            };
+
+            await using var appDbContext = new AppDbContext(SetupDefaultMoq.CreateDbContextOptions());
+            // Handler oluşturuluyor
+            _handler = new LoginCommandHandler(
+                appDbContext,
+                _signInManagerMock.Object,
+                _jwtTokenGeneratorMock.Object
+            );
+
+            await appDbContext.Users.AddAsync(userEntity);
+            await appDbContext.SaveChangesAsync();
+
+            _signInManagerMock
+                .Setup(x => x.PasswordSignInAsync(It.IsAny<UserEntity>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>()))
+                .ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.Success);
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.StatusCode, Is.EqualTo(400));
+            Assert.AreEqual(result.messages, "User not found");
+
+
+
         }
     }
 
