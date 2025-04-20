4ed99476701581c6257f093332c4e366b94c96fe
OmerSoydemir <omerfaruksoydemirr@gmail.com>
2025-04-20 19:44:08 +0300

    GetDepartment,GetUniversity,GetFaculty query ve controllerlar yazıldı.

---
+++ EveryDaily.Application/Dtos/Constants/Response/GetDepartmentsResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveryDaily.Application.Dtos.Constants.Response
+{
+    public class GetDepartmentsResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
---
+++ EveryDaily.Application/Services/ControllerCommands/Constants/Query/GetDepartmentQuery.cs
@@ -0,0 +1,34 @@
+﻿using EveryDaily.Application.Dtos.Constants.Response;
+using EveryDaily.Core.Dtos;
+using EveryDaily.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EveryDaily.Application.Services.ControllerCommands.Constants.Querie
+{
+    public class GetDepartmentQuery : IRequest<Response<List<GetDepartmentsResponse>>>
+    {
+        public Guid FacultyId { get; set; }
+    }
+
+    public class GetDepartmentQueryHandler(AppDbContext dbContext) : IRequestHandler<GetDepartmentQuery, Response<List<GetDepartmentsResponse>>>
+    {
+        public async Task<Response<List<GetDepartmentsResponse>>> Handle(GetDepartmentQuery request, CancellationToken cancellationToken)
+        {
+            var departments = await dbContext.Departments
+                .Where(d => d.FacultyId == request.FacultyId)
+                .Select(d => new GetDepartmentsResponse
+                {
+                    Id = d.Id,
+                    Name = d.Name
+                })
+                .ToListAsync(cancellationToken);
+
+            return Response<List<GetDepartmentsResponse>>.Success(departments, 200);
+        }
+    }
+}
