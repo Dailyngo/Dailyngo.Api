22bb68b6381148ae5fc6597a6fe4d91d0f4cd88a
burakhan.kurt1@gmail.com <burakhan.kurt1@gmail.com>
2025-04-23 22:12:34 +0300

    hotfix

--- EveryDaily.Application/Services/Notification/NotificationService.cs
+++ EveryDaily.Application/Services/Notification/NotificationService.cs
@@ -24,7 +24,8 @@ namespace EveryDaily.Application.Services.Notification
                 SenderId = userId,
                 Type = type,
                 RelatedEntityId = relatedEntityId,
-                IsDeleted = false
+                IsDeleted = false,
+                CreatedAt = DateTime.UtcNow,
             };
 
             await mongoDocContext.Notifications.Collection.InsertOneAsync(notification, new(), cancellationToken);
