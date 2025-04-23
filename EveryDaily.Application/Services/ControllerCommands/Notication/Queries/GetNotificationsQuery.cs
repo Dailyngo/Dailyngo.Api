406326e60913dab1f9575ab17c3c7373b7195050
burakhan.kurt1@gmail.com <burakhan.kurt1@gmail.com>
2025-04-23 22:07:25 +0300

    hotfix

--- EveryDaily.Application/Services/ControllerCommands/Notication/Queries/GetNotificationsQuery.cs
+++ EveryDaily.Application/Services/ControllerCommands/Notication/Queries/GetNotificationsQuery.cs
@@ -70,7 +70,7 @@ public class GetNotificationsQueryHandler(
                 SenderId = Guid.Parse(n.SenderId),
                 SenderName = senderNames.FirstOrDefault(sn => sn.Id == Guid.Parse(n.SenderId))?.FullName,
                 RelatedEntityId = n.RelatedEntityId,
-                CreatedAt = n.CreatedAt.Value
+                CreatedAt = n.CreatedAt
             })
             .OrderByDescending(n => n.CreatedAt)
             .ToList();
@@ -93,7 +93,7 @@ public class GetNotificationsQueryHandler(
                         ? FormatComment(comments.FirstOrDefault(c => c.Id == ObjectId.Parse(n.RelatedEntityId)))
                         : null,
                     NotificationType = n.Type,
-                    CreatedAt = n.CreatedAt.Value
+                    CreatedAt = n.CreatedAt
                 };
             })
             .Where(n => n.SenderId != Guid.Empty)
