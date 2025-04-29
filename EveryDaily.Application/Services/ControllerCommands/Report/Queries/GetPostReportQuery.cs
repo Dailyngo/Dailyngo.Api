c52e945b57b915cb62a422fbf489a784a2a852aa
İbrahim Halil Ateş <atesibrahim612@gmail.com>
2025-04-29 23:49:37 +0300

    hotfix

--- EveryDaily.Application/Services/ControllerCommands/Report/Queries/GetPostReportQuery.cs
+++ EveryDaily.Application/Services/ControllerCommands/Report/Queries/GetPostReportQuery.cs
@@ -1,6 +1,7 @@
 using EveryDaily.Application.Dtos.Report.Response;
 using EveryDaily.Core.Dtos;
 using EveryDaily.Domain.Documents.Post;
+using EveryDaily.Persistence;
 using EveryDaily.Persistence.MongoContext;
 using MediatR;
 using MongoDB.Driver;
@@ -12,7 +13,7 @@ public class GetPostReportQuery : IRequest<Response<List<PostReportResponse>>>
     public bool IsProcess { get; set; }
 }
 
-public class GetPostReportQueryHandler(MongoDocContext mongoDocContext) 
+public class GetPostReportQueryHandler(MongoDocContext mongoDocContext,AppDbContext appDbContext) 
     : IRequestHandler<GetPostReportQuery, Response<List<PostReportResponse>>>
 {
     public async Task<Response<List<PostReportResponse>>> Handle(GetPostReportQuery request, CancellationToken cancellationToken)
@@ -24,12 +25,28 @@ public class GetPostReportQueryHandler(MongoDocContext mongoDocContext)
             .SortByDescending(x => x.CreatedAt)
             .ToListAsync(cancellationToken: cancellationToken);
         
+        var userIds = postReports.Select(y => Guid.Parse(y.UserId)).ToList();
+        var users = appDbContext.Users
+            .Where(x => userIds.Contains(x.Id) && !x.IsDeleted)
+            .Select(x => new IdNameResponse<Guid>()
+            {
+                Id = x.Id,
+                Name = x.FullName
+            })
+            .ToList();
+        
         var postReportResponses = postReports.Select(x => new PostReportResponse
         {
             Id = x.Id.ToString(),
             PostId = x.PostId.ToString(),
             Reason = x.ReportReason,
-            IsProcess = x.IsProcess
+            IsProcess = x.IsProcess,
+            ReportedBy = users.FirstOrDefault(u => u.Id == Guid.Parse(x.UserId)) 
+                ?? new IdNameResponse<Guid>()
+                {
+                    Id = Guid.Empty,
+                    Name = "Bilinmeyen Kullanıcı"
+                },
         }).ToList();
         
         return Response<List<PostReportResponse>>.Success(postReportResponses, 200);
