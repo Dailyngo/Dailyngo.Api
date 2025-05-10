55acc3b439218a2609b6386192d49a4a727fd462
İbrahim Halil Ateş <atesibrahim612@gmail.com>
2025-05-10 19:46:29 +0300

    hotfix

--- EveryDaily.Application/Services/ControllerCommands/Post/Commands/CreatePostCommand.cs
+++ EveryDaily.Application/Services/ControllerCommands/Post/Commands/CreatePostCommand.cs
@@ -29,6 +29,9 @@ public class CreatePostCommandHandler(
     public async Task<Core.Dtos.Response<NoContent>> Handle(CreatePostCommand request, CancellationToken cancellationToken)
     {
         var userId = userService.GetUserId().ToString();
+        
+        if (string.IsNullOrEmpty(request.Data.Content) && string.IsNullOrEmpty(request.Data.ImageKey))
+            return Core.Dtos.Response<NoContent>.Fail(PostErrorMessage.ContentOrImageRequired, 400);
 
         if (request.Data.Id != null)
         {
